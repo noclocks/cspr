21b4db98cc38826796d91451c1660977a0f594da
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2025-02-27 20:35:59 -0500

    feat: package

---
+++ R/zzz.R
@@ -0,0 +1,16 @@
+
+#  ------------------------------------------------------------------------
+#
+# Title : onLoad & onAttach
+#    By : Jimmy Briggs
+#  Date : 2025-02-27
+#
+#  ------------------------------------------------------------------------
+
+.onLoad <- function(libname, pkgname) {
+
+}
+
+.onAttach <- function(libname, pkgname) {
+
+}
